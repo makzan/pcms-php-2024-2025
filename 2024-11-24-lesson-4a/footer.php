2d8bd421779ef31f7638fd898c26b82d749325eb
Thomas Seng Hin Mak <mak@makzan.net>
2024-11-24 09:58:02 +0800

    First version of divide and conquer code.

---
+++ 2024-11-24-lesson-4a/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    &copy; 2024 Just a demo.
+</footer>
+</body>
+</html>
---
+++ 2024-11-24-lesson-4a/index.php
@@ -0,0 +1,8 @@
+<?php include "header.php"; ?>
+<?php include "menu.php"; ?>
+
+<main>
+    This is the home page.
+</main>
+
+<?php include "footer.php"; ?>
